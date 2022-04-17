d3705dd370605d9f1a2e4d6161668e845b9b4138
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2022-04-17 15:01:34 +0100

    Code formatting for readability, no radar content changes

--- application.js
+++ application.js
@@ -9,10 +9,10 @@ radarApplicationData = {
   },
   title: "Tech Radar - Application - April 2022",
   quadrants: [
-    { name: "Platform tools" },   // 0
-    { name: "Other" },            // 1
-    { name: "Languages" },        // 2
-    { name: "Persistence" },      // 3
+    { name: "Platform tools" }, // 0
+    { name: "Other" },          // 1
+    { name: "Languages" },      // 2
+    { name: "Persistence" },    // 3
   ],
   rings: [
     { name: "MAINSTREAM", color: "#93c47d" },
@@ -21,40 +21,39 @@ radarApplicationData = {
     { name: "AVOID", color: "#efafa9" },
   ],
   print_layout: true,
-  //zoomed_quadrant: 1,
-  //ENTRIES
   entries: [
-    { "quadrant": 2, "ring": 0, "label": ".Net Core", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 3, "label": ".Net Framework", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 2, "label": "Angular.js", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 1, "label": "Erlang", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 1, "label": "Golang", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 0, "label": "Java Spring", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 3, "label": "Java EE", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 0, "label": "Node js", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 3, "label": "PHP", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 0, "label": "Python", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 1, "label": "R", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 0, "label": "React.js", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 1, "label": "Ruby", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 2, "ring": 0, "label": "Vue.js", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    // Platform tools
     { "quadrant": 0, "ring": 0, "label": "Amazon EKS", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 0, "label": "AWS Lambda", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 0, "label": "Azure AKS", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 0, "label": "Azure VMs", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 0, "label": "Docker", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 0, "ring": 3, "label": "HP-UX", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 0, "ring": 3, "label": "Kubernetes", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 0, "ring": 0, "label": "Terraform", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 2, "label": "Linux - Apache", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 2, "label": "Linux - Ubuntu", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 2, "label": "Rancher", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 0, "ring": 3, "label": "RHEL", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 0, "ring": 0, "label": "Terraform", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 2, "label": "Ubuntu", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 0, "ring": 2, "label": "VMWare", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 2, "label": "VMWare ESX", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
     { "quadrant": 0, "ring": 2, "label": "VMWARe NSX", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
-    { "quadrant": 0, "ring": 2, "label": "VMWare on AWS", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 }
+    { "quadrant": 0, "ring": 2, "label": "VMWare on AWS", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 0, "ring": 2, "label": "VMWare", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 0, "ring": 3, "label": "HP-UX", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 0, "ring": 3, "label": "Kubernetes", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 0, "ring": 3, "label": "RHEL", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    // Languages
+    { "quadrant": 2, "ring": 0, "label": ".Net Core", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 0, "label": "Java Spring", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 0, "label": "Node js", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 0, "label": "Python", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 0, "label": "React.js", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 0, "label": "Vue.js", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 1, "label": "Erlang", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 1, "label": "Golang", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 1, "label": "R", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 1, "label": "Ruby", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 2, "label": "Angular.js", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 3, "label": ".Net Framework", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 3, "label": "Java EE", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
+    { "quadrant": 2, "ring": 3, "label": "PHP", "active": false, "link": "../data_processing/aws_emr.html", "moved": 0 },
   ]
-  //ENTRIES
 };
